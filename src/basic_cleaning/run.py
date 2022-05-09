01f77132edc1009c239caca0741410c370007058
Victor Nascimento <victormatheusnasc@gmail.com>
2022-05-08 22:08:08 -0300

    Drop rows that are not in in the proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -36,6 +36,10 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
     
+    logger.info(f'Droping rows in the dataset that are not in the proper geolocation')
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     logger.info(f'Saving cleaning df and sendng to W&B')
     # Saving the dataframe in a file
     df.to_csv("clean_sample.csv", index=False)
